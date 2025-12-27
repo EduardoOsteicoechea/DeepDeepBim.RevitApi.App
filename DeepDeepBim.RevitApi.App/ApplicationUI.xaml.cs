162f79f7a171115fec4ac1d2aee03512a7df9f0b
eduar <eduar@EDUARDO>
2025-12-27 11:05:24 -0400

    Incorporating ExternalCommandData for tests

--- DeepDeepBim.RevitApi.App/ApplicationUI.xaml.cs
+++ DeepDeepBim.RevitApi.App/ApplicationUI.xaml.cs
@@ -1,11 +1,16 @@
 ﻿using System.Windows.Controls;
+using Autodesk.Revit.UI;
 
 namespace DeepDeepBim.RevitApi.App;
 
 public partial class ApplicationUI : UserControl
 {
-	public ApplicationUI()
+	public ApplicationUI(ExternalCommandData commandData)
 	{
 		InitializeComponent();
+
+		string message = string.Empty;
+
+		new RevitApiAccessibilityTestCommand().Execute(commandData, ref message, null!);
 	}
 }
